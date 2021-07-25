8d2a123d2da1e9396c9b7dc21032b8b40bc23e0a
chicken-developer <quynh.fullstackdev@gmail.com>
2021-07-26 05:59:23 +0700

    Config get all method for user

--- project/backend/src/main/scala/CampRestful/User/UserLogic.scala
+++ project/backend/src/main/scala/CampRestful/User/UserLogic.scala
@@ -5,9 +5,10 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.model.{HttpEntity, StatusCode, StatusCodes}
 import akka.stream.Materializer
 import org.bson.types.ObjectId
-import org.mongodb.scala.bson.Document
+import org.mongodb.scala.bson.{BsonObjectId, Document}
+import org.mongodb.scala.model.Filters
 import org.mongodb.scala.model.Filters.equal
-import org.mongodb.scala.model.Updates.set
+import org.mongodb.scala.model.Updates.{combine, currentDate, set}
 
 import scala.collection.convert.ImplicitConversions.`iterable AsScalaIterable`
 import scala.concurrent.Future
@@ -26,7 +27,6 @@ case object UserLogic {
       .map { user =>
         UserLogic.ConvertToUser(user.toString.replaceAll("Document", "User"))
       }.toList
-
     val user = allUsers.findLast(_.username == usrname).filter(_.password == pass)
     user match {
       case Some(user) =>
@@ -37,32 +37,37 @@ case object UserLogic {
 
   def HandleUserRegister(user: User): Future[StatusCode] = {
     userCollection.insertOne(UserLogic.DocumentFromUser(user))
-    val temp = Future(StatusCodes.OK)
-    temp
+    Future(StatusCodes.OK)
   }
-  def HandleUserUpdateData(user: User, userId: String): Future[StatusCode] = {
-   // userCollection.updateOne(equal("username", 10), set("i", 110))
-    val temp = Future(StatusCodes.OK)
-    temp
+
+  def HandleUserUpdateData(newData: User, userId: String): Future[User] = {
+    val allUsers = userCollection.find()
+      .map { user =>
+        UserLogic.ConvertToUser(user.toString.replaceAll("Document", "User"))
+      }.toList
+    val user = allUsers.findLast(_._id == userId) match {
+      case Some(usr) =>
+        val updateDocument = UserLogic.DocumentFromUserForUpdate(oldUser = usr, newUser = newData)
+        userCollection.replaceOne(equal("_id", userId),updateDocument)
+        usr
+      case None => templateUser
+    }
+    Future(user)
   }
 
   def HandleDeleteUser(userId: String): Future[StatusCode] = {
-    // userCollection.updateOne(equal("username", 10), set("i", 110))
-    val temp = Future(StatusCodes.OK)
-    temp
+    userCollection.deleteOne(equal("_id", userId))
+    Future(StatusCodes.OK)
   }
+
   def GetAllUser(): Future[List[User]] = {
-
-    //TODO
-    val temp = Future(List[User](templateUser,templateUser,templateUser,templateUser))
-    temp
-  }
-
-  def FindUserInDatabase(entity: HttpEntity): Future[User] = {
-
-    val strictEntityFuture = entity.toStrict(2 seconds)
-    val temp = Future(List[User](templateUser).head)
-    temp
+    val allUsers = userCollection.find()
+      .map { user =>
+        println("==============================User:")
+        println(user.toString.replaceAll("Document", "User"))
+        UserLogic.ConvertToUser(user.toString.replaceAll("Document", "User"))
+      }.toList.filter(user => user._id != templateUser._id)
+    Future(allUsers)
   }
 
   def GetUserById(id: String): Future[User] = {
@@ -83,7 +88,7 @@ case object UserLogic {
       .replace("User{{","")
       .replace("}}","")
     match {
-      case s"_id=$id, bookingHistoryId=$arrBooking, email=$email, firstName=$f_name, lastName=$l_name, password=$password, phoneNumber=$phoneNumber, typeOfUser=$typeOfUser, username=$username" =>
+      case s"_id=$id, username=$username, typeOfUser=$typeOfUser, firstName=$f_name, lastName=$l_name, password=$password, email=$email, phoneNumber=$phoneNumber, bookingHistoryId=$arrBooking" =>
         val bookingHistory = arrBooking.replace("[", "").replace("]", "").split(",").toList
         println(s"After format: ${User(id, username,typeOfUser, f_name,l_name,password,email,phoneNumber,bookingHistory)}")
         User(id, username,typeOfUser, f_name,l_name,password,email,phoneNumber,bookingHistory)
@@ -103,5 +108,16 @@ case object UserLogic {
       "bookingHistoryId" -> user.bookingHistoryId)
   }
 
+  def DocumentFromUserForUpdate(oldUser: User, newUser: User): Document = {
+    Document("_id" -> oldUser._id,
+      "username" -> newUser.username,
+      "typeOfUser" -> newUser.typeOfUser,
+      "firstName" -> newUser.firstName,
+      "lastName" -> newUser.lastName,
+      "password" -> newUser.password,
+      "email"-> newUser.email,
+      "phoneNumber" -> newUser.phoneNumber,
+      "bookingHistoryId" -> oldUser.bookingHistoryId)
+  }
 
 }
--- project/backend/src/main/scala/CampRestful/User/UserRoute.scala
+++ project/backend/src/main/scala/CampRestful/User/UserRoute.scala
@@ -15,203 +15,244 @@ import scala.util.{Failure, Success}
 class UserRoute(implicit val actorSystem : ActorSystem, implicit  val actorMaterializer: Materializer) extends Directives{
   import Routes.Data._
   import ch.megard.akka.http.cors.scaladsl.CorsDirectives._
-  val userFinalRoute: Route = cors() {
-    pathPrefix("api_v01" / "user") {
-        (post & pathPrefix("login") & extractRequest) { request =>
-          //POST user -> for user register
-          case class UserForm(username: String, password: String)
-          implicit val userFormFormat = jsonFormat2(UserForm)
-          val entity = request.entity
-          val strictEntityFuture = entity.toStrict(2 seconds)
-          val loginForm = strictEntityFuture.map(_.data.utf8String.parseJson.convertTo[UserForm])
-          println(s"Data from server: $loginForm")
+  val userRoute: Route = cors() {
+    (post & path("api_v01" / "user" / "login") & extractRequest) { request =>
+      case class UserForm(username: String, password: String)
+      implicit val userFormFormat = jsonFormat2(UserForm)
+      val entity = request.entity
+      val strictEntityFuture = entity.toStrict(2 seconds)
+      val loginForm = strictEntityFuture.map(_.data.utf8String.parseJson.convertTo[UserForm])
+      println(s"Data from server: $loginForm")
 
-          onComplete(loginForm) {
+      onComplete(loginForm) {
+        case Failure(ex) =>
+          complete(
+            StatusCodes.InternalServerError,
+            HttpEntity(
+              ContentTypes.`application/json`,
+              Message("Input not valid", 0, "".toJson).toJson.prettyPrint
+            )
+          )
+        case Success(form) => //Write user to database if valid
+          val userFuture = UserLogic.GetUserFromLoginForm(form.username, form.password)
+          onComplete(userFuture) {
             case Failure(ex) =>
-              complete (
+              complete(
                 StatusCodes.InternalServerError,
-                  HttpEntity(
+                HttpEntity(
                   ContentTypes.`application/json`,
-                  Message("Input not valid", 0, "".toJson).toJson.prettyPrint
+                  Message("Can't handle input", 0, "".toJson).toJson.prettyPrint
                 )
               )
-            case Success(form) => //Write user to database if valid
-              val userFuture = UserLogic.GetUserFromLoginForm(form.username, form.password)
-              onComplete(userFuture) {
-                case Failure(ex) =>
-                  complete (
-                    StatusCodes.InternalServerError,
-                    HttpEntity(
-                      ContentTypes.`application/json`,
-                      Message("Can't handle input", 0, "".toJson).toJson.prettyPrint
-                    )
-                  )
-                case Success(confirmUser) =>
-                  if(confirmUser.username == templateUser.username)
-                    {
-                      complete (
-                        StatusCodes.InternalServerError,
-                        HttpEntity(
-                          ContentTypes.`application/json`,
-                          Message("Wrong username or password", 0, "".toJson).toJson.prettyPrint
-                        )
-                      )
-                    }
-                  else
-                    {
-                      complete {
-                        HttpEntity(
-                          ContentTypes.`application/json`,
-                          Message("Success", 1, confirmUser.toJson).toJson.prettyPrint
-                        )
-                      }
-                    }
-
-              }
-          }
-        } ~
-        (post & pathPrefix("register") & extractRequest) { request =>
-            case class RegisterForm(username: String,firstName: String, lastName: String, password: String, email: String, phoneNumber: String)
-            implicit val registerFormFormat = jsonFormat6(RegisterForm)
-            val entity = request.entity
-            val strictEntityFuture = entity.toStrict(2 seconds)
-            val registerForm = strictEntityFuture.map(_.data.utf8String.parseJson.convertTo[RegisterForm])
-            println(s"Data from server: $registerForm")
-
-            onComplete(registerForm) {
-              case Failure(ex) =>
-                complete (
-                  StatusCodes.InternalServerError,
-                    HttpEntity(
-                    ContentTypes.`application/json`,
-                    Message("Input not valid", 0, "".toJson).toJson.prettyPrint
-                  )
-                )
-              case Success(form) => //Write user to database if valid
-                val user: User = User("id_null", form.username, "normal", form.firstName, form.lastName, form.password,form.email, form.phoneNumber, List(""))
-                val userFuture = UserLogic.HandleUserRegister(user)
-                onComplete(userFuture) {
-                  case Failure(ex) =>
-                    complete (
-                      StatusCodes.InternalServerError,
-                        HttpEntity(
-                        ContentTypes.`application/json`,
-                        Message("Fail to register", 0, "".toJson).toJson.prettyPrint
-                      )
-                    )
-                  case Success(valueNotUse) =>
-                    complete {
-                      HttpEntity(
-                        ContentTypes.`application/json`,
-                        Message("Success", 1, user.toJson).toJson.prettyPrint
-                      )
-                    }
-                }
-            }
-          } ~
-        (put & pathPrefix(Segment) & extractRequest) { (userId, request) =>
-            case class NewData(userId: String, username: String,firstName: String, lastName: String, password: String, email: String, phoneNumber: String)
-            implicit val updateFormat = jsonFormat7(NewData)
-            val entity = request.entity
-            val strictEntityFuture = entity.toStrict(2 seconds)
-            val newDataForm = strictEntityFuture.map(_.data.utf8String.parseJson.convertTo[NewData])
-            println(s"Data from server: $newDataForm")
-
-            onComplete(newDataForm) {
-              case Failure(ex) =>
-                complete (
+            case Success(confirmUser) =>
+              if (confirmUser.username == templateUser.username) {
+                complete(
                   StatusCodes.InternalServerError,
                   HttpEntity(
                     ContentTypes.`application/json`,
-                    Message("New data not valid", 0, "".toJson).toJson.prettyPrint
+                    Message("Wrong username or password", 0, "".toJson).toJson.prettyPrint
                   )
                 )
-              case Success(form) => //Write user to database if valid
-                val user: User = User(form.userId, form.username, "normal", form.firstName, form.lastName, form.password,form.email, form.phoneNumber, List(""))
-                val userFuture = UserLogic.HandleUserUpdateData(user, user.typeOfUser)
-                onComplete(userFuture) {
-                  case Failure(ex) =>
-                    complete (
-                      StatusCodes.InternalServerError,
-                      HttpEntity(
-                        ContentTypes.`application/json`,
-                        Message("Fail to update", 0, "".toJson).toJson.prettyPrint
-                      )
-                    )
-                  case Success(valueNotUse) =>
-                    complete {
-                      HttpEntity(
-                        ContentTypes.`application/json`,
-                        Message("Success", 1, user.toJson).toJson.prettyPrint
-                      )
-                    }
-                }
-            }
-          } ~
-        (delete &  pathPrefix(Segment)) { userId =>
-            val userFuture = UserLogic.HandleDeleteUser(userId)
-            onComplete(userFuture) {
-              case Success(user) =>
+              }
+              else {
                 complete {
                   HttpEntity(
                     ContentTypes.`application/json`,
-                    Message("Success",1,"".toJson).toJson.prettyPrint
+                    Message("Success", 1, confirmUser.toJson).toJson.prettyPrint
                   )
                 }
-              case Failure(ex) =>
-                complete (
-                  StatusCodes.InternalServerError,
-                  HttpEntity(
-                    ContentTypes.`application/json`,
-                    Message("Fail to delete", 0, "".toJson).toJson.prettyPrint
-                  )
-                )
-            }
-          } ~
-        (get &  pathPrefix("all")) {
-        val userFuture = UserLogic.GetAllUser()
-        onComplete(userFuture) {
-          case Success(user) =>
-            complete {
-              HttpEntity(
-                ContentTypes.`application/json`,
-                Message("Success",1,user.toJson).toJson.prettyPrint
-              )
-            }
+              }
+
+          }
+      }
+    } ~
+    (post & path("api_v01" / "user" / "register") & extractRequest) { request =>
+        case class RegisterForm(username: String, firstName: String, lastName: String, password: String, email: String, phoneNumber: String)
+        implicit val registerFormFormat = jsonFormat6(RegisterForm)
+        val entity = request.entity
+        val strictEntityFuture = entity.toStrict(2 seconds)
+        val registerForm = strictEntityFuture.map(_.data.utf8String.parseJson.convertTo[RegisterForm])
+        println(s"Data from server: $registerForm")
+
+        onComplete(registerForm) {
           case Failure(ex) =>
-            complete (
+            complete(
               StatusCodes.InternalServerError,
               HttpEntity(
                 ContentTypes.`application/json`,
-                Message("Fail to get all user", 0, "".toJson).toJson.prettyPrint
+                Message("Input not valid", 0, "".toJson).toJson.prettyPrint
+              )
+            )
+          case Success(form) => //Write user to database if valid
+            val user: User = User("id_null", form.username, "normal", form.firstName, form.lastName, form.password, form.email, form.phoneNumber, List(""))
+            val userFuture = UserLogic.HandleUserRegister(user)
+            onComplete(userFuture) {
+              case Failure(ex) =>
+                complete(
+                  StatusCodes.InternalServerError,
+                  HttpEntity(
+                    ContentTypes.`application/json`,
+                    Message("Fail to register", 0, "".toJson).toJson.prettyPrint
+                  )
+                )
+              case Success(valueNotUse) =>
+                complete {
+                  HttpEntity(
+                    ContentTypes.`application/json`,
+                    Message("Success", 1, user.toJson).toJson.prettyPrint
+                  )
+                }
+            }
+        }
+      } ~
+    (put & path("api_v01" / "user" / "update" / Segment) & extractRequest) { (userId, request) =>
+        case class NewData(username: String, firstName: String, lastName: String, password: String, email: String, phoneNumber: String)
+        implicit val updateFormat = jsonFormat6(NewData)
+        val entity = request.entity
+        val strictEntityFuture = entity.toStrict(2 seconds)
+        val newDataForm = strictEntityFuture.map(_.data.utf8String.parseJson.convertTo[NewData])
+        println(s"Data from server: $newDataForm")
+
+        onComplete(newDataForm) {
+          case Failure(ex) =>
+            complete(
+              StatusCodes.InternalServerError,
+              HttpEntity(
+                ContentTypes.`application/json`,
+                Message("New data not valid", 0, "".toJson).toJson.prettyPrint
+              )
+            )
+          case Success(form) => //Write user to database if valid
+            val userDataTemplate: User = User(userId, form.username, "normal", form.firstName, form.lastName, form.password, form.email, form.phoneNumber, List(""))
+            val userFuture = UserLogic.HandleUserUpdateData(userDataTemplate, userId)
+            onComplete(userFuture) {
+              case Failure(ex) =>
+                complete(
+                  StatusCodes.InternalServerError,
+                  HttpEntity(
+                    ContentTypes.`application/json`,
+                    Message("Fail to update", 0, "".toJson).toJson.prettyPrint
+                  )
+                )
+              case Success(userWithNewData) =>
+                if(userWithNewData._id == templateUser._id ) {
+                  complete {
+                    HttpEntity(
+                      ContentTypes.`application/json`,
+                      Message("Not have user", 0, "".toJson).toJson.prettyPrint
+                    )
+                  }
+                } else {
+                  complete {
+                    HttpEntity(
+                      ContentTypes.`application/json`,
+                      Message("Success", 1, userWithNewData.toJson).toJson.prettyPrint
+                    )
+                  }
+                }
+            }
+        }
+      } ~
+    (delete & path("api_v01" / "user" / Segment)) { userId =>
+      val userFuture = UserLogic.HandleDeleteUser(userId)
+      onComplete(userFuture) {
+        case Success(user) =>
+          complete {
+            HttpEntity(
+              ContentTypes.`application/json`,
+              Message("Success", 1, "".toJson).toJson.prettyPrint
+            )
+          }
+        case Failure(ex) =>
+          complete(
+            StatusCodes.InternalServerError,
+            HttpEntity(
+              ContentTypes.`application/json`,
+              Message("Fail to delete", 0, "".toJson).toJson.prettyPrint
+            )
+          )
+      }
+    } ~
+    (get & path("api_v01" / "user" / Segment)) { userId =>
+      userId match {
+        case "all" =>
+          val userFuture = UserLogic.GetAllUser()
+          onComplete(userFuture) {
+            case Success(user) =>
+              complete {
+                HttpEntity(
+                  ContentTypes.`application/json`,
+                  Message(s"Success get ${user.size}", 1, user.toJson).toJson.prettyPrint
+                )
+              }
+            case Failure(ex) =>
+              complete(
+                StatusCodes.InternalServerError,
+                HttpEntity(
+                  ContentTypes.`application/json`,
+                  Message("Fail to get all user", 0, "".toJson).toJson.prettyPrint
+                )
+              )
+          }
+        case _ =>
+          val user = UserLogic.GetUserById(userId)
+          onComplete(user) {
+            case Failure(ex) =>
+              complete(
+                StatusCodes.InternalServerError,
+                HttpEntity(
+                  ContentTypes.`application/json`,
+                  Message(s"Not have user with _id: $userId", 0, "".toJson).toJson.prettyPrint
+                )
+              )
+            case Success(confirmUser) =>
+              if (confirmUser.username == templateUser.username) {
+                complete(
+                  StatusCodes.InternalServerError,
+                  HttpEntity(
+                    ContentTypes.`application/json`,
+                    Message("User data not valid", 0, "".toJson).toJson.prettyPrint
+                  )
+                )
+              }
+              else {
+                complete {
+                  HttpEntity(
+                    ContentTypes.`application/json`,
+                    Message("Success", 1, confirmUser.toJson).toJson.prettyPrint
+                  )
+                }
+              }
+          }
+      }
+      }
+    }
+  val historyRoute: Route = cors() {
+    pathPrefix("api_v01" / "history") {
+      (get & pathPrefix(Segment)) { userid =>
+        val historyBookingOfUser = BookingLogic.GetAllBookingForHistory(userid)
+        onComplete(historyBookingOfUser) {
+          case Success(allBookingHistory) =>
+            val finalData = allBookingHistory.filter(booking => booking._id != templateBooking._id)
+            complete {
+              HttpEntity(
+                ContentTypes.`application/json`,
+                Message("Success", 1, finalData.toJson).toJson.prettyPrint
+              )
+            }
+          case Failure(ex) =>
+            complete(
+              StatusCodes.InternalServerError,
+              HttpEntity(
+                ContentTypes.`application/json`,
+                Message("Fail to get history", 0, "".toJson).toJson.prettyPrint
               )
             )
         }
       }
-    } ~// Finish user path prefix
-    pathPrefix("api_v01"/ "history") {
-      (get &  pathPrefix(Segment)) { userid =>
-          val historyBookingOfUser = BookingLogic.GetAllBookingForHistory(userid)
-          onComplete(historyBookingOfUser) {
-            case Success(allBookingHistory) =>
-              val finalData = allBookingHistory.filter(booking => booking._id != templateBooking._id)
-              complete {
-                HttpEntity(
-                  ContentTypes.`application/json`,
-                  Message("Success",1,finalData.toJson).toJson.prettyPrint
-                )
-              }
-            case Failure(ex) =>
-              complete (
-                StatusCodes.InternalServerError,
-                HttpEntity(
-                  ContentTypes.`application/json`,
-                  Message("Fail to get history", 0, "".toJson).toJson.prettyPrint
-                )
-              )
-          }
-        }
-    } ~
+    }
+  }
+  val bookingRoute: Route = cors() {
     pathPrefix("api_v01"/ "booking") {
       (post & pathPrefix(Segment) & extractRequest ) { (username, request) =>
         val entity = request.entity
@@ -250,5 +291,6 @@ class UserRoute(implicit val actorSystem : ActorSystem, implicit  val actorMater
         }
       }
     }
-  }// Finish user route
+  }
+  val userFinalRoute = cors(){ userRoute ~ historyRoute ~ bookingRoute }
 }
